c7bc8c72a6d7e4b461ed2a347459a61f340d159f
Naveed Akhtar <balochmna188@gmail.com>
2024-12-29 20:15:27 +0500

    update

--- src/app/page.js
+++ src/app/page.js
@@ -7,7 +7,6 @@ import Link from "next/link";
 export default function Home() {
   return (
     <div className={styles.page}>
-    <Link href="./auth/dashboard">Dashboard</Link>
      <Link href="/auth/signup">Signup</Link>
     <Link href="./auth/login">Login</Link>
     </div>
